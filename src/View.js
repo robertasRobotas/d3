3f4e99624be09328c49c9b307c39fcd3f293c539
Robertas <r.ankudovicius@gmail.com>
2020-04-30 09:19:14 +0300

    some changes

--- src/View.js
+++ src/View.js
@@ -13,15 +13,17 @@ const View = () => {
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var geometry = new THREE.BoxGeometry(1, 2, 1);
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
     var animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cube.rotation.x -= 0.04;
+      cube.rotation.y += 0.05;
+      cube.rotation.z += 0.02;
+
       renderer.render(scene, camera);
     };
     animate();
